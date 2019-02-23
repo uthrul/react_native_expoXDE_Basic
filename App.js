f0f21e345ce5f0f449a9fa41e8b0a22922df712c
saud maruli <uthrul@gmail.com>
2019-02-23 11:36:26 +0700

    props and add Image

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+
+    // contoh props 1
+    let pic = {
+      uri: 'https://bloximages.newyork1.vip.townnews.com/thesuburban.com/content/tncms/assets/v3/editorial/b/1c/b1c50de4-6a97-11e8-b5db-37ed72466eb6/5b19a0390b4bc.image.jpg?resize=400%2C228'
+    }
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Get The Shoot - Hardcore SXE </Text>
+        <Image source={pic} style={{ height: 200, width: 250 }} />
+        <Name name="STRAIGHT EDGE"></Name>
       </View>
     );
+  }
+}
+
+// contoh props 2
+class Name extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}</Text>
+    )
   }
 }
 
